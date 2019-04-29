f70f78812f55e57eedb51c7f32c20dd038bded8d
LORDBABUINO <jorge.x7@gmail.com>
2019-04-29 09:05:49 +0000

    Rolls up window on details click

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -9,4 +9,9 @@ gallerieItemList.map((item) => {
 	item.addEventListener('mouseleave', () => {
 		item.classList.toggle('gallerie__item--hover')
 	})
+
+	const button = item.querySelector('button')
+	button.addEventListener('click', () => {
+		window.scrollTo(0,0)
+	})
 })
