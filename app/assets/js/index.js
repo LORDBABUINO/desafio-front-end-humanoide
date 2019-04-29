47117c6761ffcff07e58e33ab27ca15fce89533e
LORDBABUINO <jorge.x7@gmail.com>
2019-04-29 15:24:42 +0000

    Adds first implementetion of details section (incomplete)

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -2,16 +2,19 @@ const gallerieItemList = Array.from(
 	document.getElementsByClassName('gallerie__item')
 )
 
-gallerieItemList.map((item) => {
+gallerieItemList.map((item, index) => {
+
 	item.addEventListener('mouseenter', () => {
 		item.classList.toggle('gallerie__item--hover')
 	})
+
 	item.addEventListener('mouseleave', () => {
 		item.classList.toggle('gallerie__item--hover')
 	})
 
-	const button = item.querySelector('button')
-	button.addEventListener('click', () => {
+	item.querySelector('button').addEventListener('click', () => {
 		window.scrollTo(0,0)
+		document.querySelector('.banner').classList.add('banner--disabled')
+		document.querySelector(`.details--${index}`).classList.add('details--enabled')
 	})
 })
