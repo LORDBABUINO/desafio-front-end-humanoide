305b095f2a18d46f6822a8d83b11f1b713b4a981
LORDBABUINO <jorge.x7@gmail.com>
2019-05-01 14:14:35 +0000

    -Corrects 'gallerie' to 'gallery' -Loads gallery from a http request -Adds scroll to gallery

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,61 +1,69 @@
 (async function main () {
+
+	const renderGallery = (productList) => {
+
+		productList.map((product) => {
+
+			const gallery = document.querySelector('.gallery')
+			const galleryItem = document.createElement('li')
+			const galleryImg = document.createElement('img')
+			const galleryPrice = document.createElement('p')
+			const galleryPriceOrange = document.createElement('b')
+			const galleryButton = document.createElement('button')
+
+			galleryItem.classList.add('gallery__item')
+			galleryImg.classList.add('gallery__img')
+			galleryPrice.classList.add('gallery__price')
+			galleryPriceOrange.classList.add('gallery__price--orange')
+			galleryButton.classList.add('button')
+			galleryButton.classList.add('gallery__button')
+
+			galleryImg.setAttribute('src', product.image)
+
+			galleryButton.innerHTML = 'Mais detalhes'
+			galleryPriceOrange.innerHTML = 'R$' + (
+				product.promotional_price
+					? product.promotional_price
+					: product.price
+			)
+
+			product.price && product.promotional_price && (() => {
+				const strike = document.createElement('strike')
+				strike.innerHTML = 'R$' + product.price
+				galleryPrice.innerHTML += 'de '
+				galleryPrice.appendChild(strike)
+				galleryPrice.innerHTML += ' '
+			})()
+
+			galleryPrice.innerHTML += 'por '
+			galleryPrice.appendChild(galleryPriceOrange)
+
+			galleryItem.appendChild(galleryImg)
+			galleryItem.appendChild(galleryPrice)
+			galleryItem.appendChild(galleryButton)
+			gallery.appendChild(galleryItem)
+
+			galleryItem.addEventListener('mouseenter', () => {
+				galleryItem.classList.toggle('gallery__item--hover')
+			})
+
+			galleryItem.addEventListener('mouseleave', () => {
+				galleryItem.classList.toggle('gallery__item--hover')
+			})
+
+			galleryItem.querySelector('button').addEventListener('click', () => {
+				window.scrollTo(0,0)
+				document.querySelector('.banner').classList.add('banner--disabled')
+				document.querySelector('.details--1').classList.add('details--enabled')
+			})
+		})
+	}
+
 	const http = new XMLHttpRequest()
 
 	http.onreadystatechange = () =>
-		http.readyState == 4 && console.log(http.response)
+		http.readyState == 4 && renderGallery(JSON.parse(http.response).products)
 
 	http.open('GET', '/server/data.json', true)
 	http.send()
-
-	const productList = [
-		{
-			price: 200.00,
-			image: '../assets/images/fantasia-01.png',
-			promotional_price: '100.00'
-		},
-		{
-			price: 200.00,
-			image: '../assets/images/fantasia-02.png',
-			promotional_price: '100.00'
-		},
-		{
-			price: 200.00,
-			image: '../assets/images/fantasia-03.png',
-			promotional_price: '100.00'
-		},
-	]
-
-	productList.map((product) => {
-
-		const gallerie = document.querySelector('.gallerie')
-		const gallerieItem = document.createElement('li')
-		const gallerieImg = document.createElement('img')
-		const galleriePrice = document.createElement('p')
-		const galleriePriceOrange = document.createElement('b')
-		const gallerieButton = document.createElement('button')
-		const strike = document.createElement('strike')
-
-		gallerieItem.classList.add('gallerie__item')
-		gallerieImg.classList.add('gallerie__img')
-		galleriePrice.classList.add('gallerie__price')
-		galleriePriceOrange.classList.add('gallerie__price--orange')
-		gallerieButton.classList.add('button')
-		gallerieButton.classList.add('gallerie__button')
-
-		gallerieImg.setAttribute('src', product.image)
-
-		gallerieButton.innerHTML = 'Mais detalhes'
-		strike.innerHTML = product.price
-		galleriePriceOrange.innerHTML = product.promotional_price
-
-		galleriePrice.innerHTML += 'de '
-		galleriePrice.appendChild(strike)
-		galleriePrice.innerHTML += ' por '
-		galleriePrice.appendChild(galleriePriceOrange)
-
-		gallerieItem.appendChild(gallerieImg)
-		gallerieItem.appendChild(galleriePrice)
-		gallerieItem.appendChild(gallerieButton)
-		gallerie.appendChild(gallerieItem)
-	})
 })()
--- app/assets/js/listeners.js
+++ app/assets/js/listeners.js
@@ -1,62 +1,64 @@
-const bannerButton = document.querySelector('.banner__button')
+(function main () {
+	const bannerButton = document.querySelector('.banner__button')
 
-bannerButton.addEventListener('click', () => {
-		window.scrollTo(0, 800)
-})
-
-const gallerieItemList = Array.from(
-	document.getElementsByClassName('gallerie__item')
-)
-
-gallerieItemList.map((item, index) => {
-
-	item.addEventListener('mouseenter', () => {
-		item.classList.toggle('gallerie__item--hover')
+	bannerButton.addEventListener('click', () => {
+			window.scrollTo(0, 800)
 	})
 
-	item.addEventListener('mouseleave', () => {
-		item.classList.toggle('gallerie__item--hover')
+	const galleryItemList = Array.from(
+		document.getElementsByClassName('gallery__item')
+	)
+
+	galleryItemList.map((item, index) => {
+
+		item.addEventListener('mouseenter', () => {
+			item.classList.toggle('gallery__item--hover')
+		})
+
+		item.addEventListener('mouseleave', () => {
+			item.classList.toggle('gallery__item--hover')
+		})
+
+		item.querySelector('button').addEventListener('click', () => {
+			window.scrollTo(0,0)
+			document.querySelector('.banner').classList.add('banner--disabled')
+			document.querySelector('.details--1').classList.add('details--enabled')
+		})
 	})
 
-	item.querySelector('button').addEventListener('click', () => {
-		window.scrollTo(0,0)
-		document.querySelector('.banner').classList.add('banner--disabled')
-		document.querySelector('.details--1').classList.add('details--enabled')
+	const sizeRadioList = Array.from(
+		document.getElementsByClassName('details__size-radio')
+	)
+
+	sizeRadioList.map((radio) => {
+		radio.addEventListener('click', () => {
+			const active = document.querySelector('.details__size-radio--active')
+			active && active.classList.toggle('details__size-radio--active')
+			radio.classList.toggle('details__size-radio--active')
+		})
 	})
-})
 
-const sizeRadioList = Array.from(
-	document.getElementsByClassName('details__size-radio')
-)
+	const detailsButton = document.querySelector('.details__button')
 
-sizeRadioList.map((radio) => {
-	radio.addEventListener('click', () => {
-		const active = document.querySelector('.details__size-radio--active')
-		active && active.classList.toggle('details__size-radio--active')
-		radio.classList.toggle('details__size-radio--active')
+	detailsButton.addEventListener('click', () => {
+		document.querySelector('.notification').classList.toggle('notification--active')
 	})
-})
 
-const detailsButton = document.querySelector('.details__button')
+	const notificationButtonContinue = document.querySelector('.notification__button--continue')
 
-detailsButton.addEventListener('click', () => {
-	document.querySelector('.notification').classList.toggle('notification--active')
-})
+	notificationButtonContinue.addEventListener('click', () => {
+		detailsButton.click()
+	})
 
-const notificationButtonContinue = document.querySelector('.notification__button--continue')
+	const notification = document.querySelector('.notification')
 
-notificationButtonContinue.addEventListener('click', () => {
-	detailsButton.click()
-})
+	notification.addEventListener('click', () => {
+		detailsButton.click()
+	})
 
-const notification = document.querySelector('.notification')
+	const notificationModal = document.querySelector('.notification__modal')
 
-notification.addEventListener('click', () => {
-	detailsButton.click()
-})
-
-const notificationModal = document.querySelector('.notification__modal')
-
-notificationModal.addEventListener('click', (event) => {
-	event.stopPropagation()
-})
+	notificationModal.addEventListener('click', (event) => {
+		event.stopPropagation()
+	})
+})()
